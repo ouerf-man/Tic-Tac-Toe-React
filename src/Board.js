f8c9834d73996febaa8c3742dbc6a82c29644538
Raed Ouerfelli <werfelli.raed@yahoo.fr>
2019-07-28 22:37:49 +0200

    2 players

--- src/Board.js
+++ src/Board.js
@@ -16,15 +16,49 @@ export default class Board extends React.PureComponent {
         this.setState({squareIndex: i})
     };
 
-
+    calculateWinner = (squares) => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
 
     handleClick(i) {
+        let flag = true;
         const squares = this.state.squares.slice();
+        if (squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
+        console.log(squares);
         this.setState({
             squares: squares,
             xIsNext: !this.state.xIsNext,
         });
+        for (let i = 0; i < squares.length; i++) {
+            if (!squares[i]) flag = false;
+        }
+
+
+
+        if (this.calculateWinner(squares)) {
+            alert(this.calculateWinner(squares) + " is the winner");
+        }
+        if (flag) {alert("game over")};
+
     }
 
 
